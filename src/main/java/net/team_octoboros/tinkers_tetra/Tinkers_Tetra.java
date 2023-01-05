27e94e5663a7217f16978b4d3ed4324a25dd08d8
Octoboros <Team.Octoboros@gmail.com>
2023-01-04 22:01:17 -0500

    Basic Setup Customization
    Made edits for actual mod setup. MDK is ready to go

---
+++ src/main/java/net/team_octoboros/tinkers_tetra/Tinkers_Tetra.java
@@ -0,0 +1,36 @@
+package net.team_octoboros.tinkers_tetra;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(Tinkers_Tetra.MOD_ID)
+public class Tinkers_Tetra
+{
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "tinkers_tetra";
+    public Tinkers_Tetra()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
